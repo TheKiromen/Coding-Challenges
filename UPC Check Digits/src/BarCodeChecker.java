dc8c2c925b34e12dfc0f555b9323e7d2bbf484ae
TheKiromen <TheKiromen@gmail.com>
2018-12-30 16:16:59 +0100

    Commit

---
+++ UPC Check Digits/src/BarCodeChecker.java
@@ -0,0 +1,13 @@
+
+public class BarCodeChecker {
+
+	public static void main(String[] args) {
+		BarCodeChecker bcc = new BarCodeChecker();
+		bcc.start();
+	}
+
+	private void start() {
+		
+	}
+	
+}
