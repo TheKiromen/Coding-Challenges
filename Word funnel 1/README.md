484f9475d468cc46f354a7a9037fc1f323bb9be6
TheKiromen <TheKiromen@gmail.com>
2019-02-07 14:14:53 +0100

    Finished Bonus2

--- Word funnel 1/README.md
+++ Word funnel 1/README.md
@@ -1,3 +1,5 @@
 # Coding Challenge #366 Word funnel 1
 
 **[Link to challenge](https://www.reddit.com/r/dailyprogrammer/comments/98ufvz/20180820_challenge_366_easy_word_funnel_1/)**
+
+Bonus2 might be a little bit confusing. I tried to comment it but basically what we are doing there is grouping words by their length and then funneling them with words which length is smaller by one. The solution isn't perfect, takes around 10 minutes to complete but I think its still better solution that comparing everything with everything.
--- Word funnel 1/src/Funneler.java
+++ Word funnel 1/src/Funneler.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Funneler {
 
@@ -17,6 +18,7 @@ public class Funneler {
 		bonus1("dragoon");
 		bonus1("boats");
 		bonus1("affidavit");
+		bonus2();
 	}
 
 	private boolean funnel(String s1,String s2) {
@@ -44,13 +46,78 @@ public class Funneler {
 			}
 			System.out.print("]\n");
 			reader.close();
-		} catch (IOException ex) {
+		}catch (IOException ex) {
 			System.out.println("Missing File");
 		}
 	}
 	
 	//--TODO--//
 	private void bonus2() {
-		return;
+		BufferedReader reader;
+		//Holds arrays of words with same length
+		ArrayList<ArrayList<String>> wordslist=new ArrayList<ArrayList<String>>();
+		//holds words with the same length
+		ArrayList<String> list;
+		String tmp;
+		//Im using them for counting how many words are there in total
+		int counter=0;
+		int words=0;
+		
+		int word_length=4;
+		try {
+			reader = new BufferedReader(new FileReader("enable1.txt"));
+			//Big file so needs lots of memory, Bad solution but i dont know better one
+			reader.mark(90000000);
+			//This loop gets amount of words
+			//We exclude words that are shorter than 4 letters since to create 5 "subwords" we will need word wich is atleast 5 letters
+			//long and we will get five 4-letter subwords
+			while((tmp=reader.readLine())!=null) {
+				if(tmp.length()>=word_length) {
+					words++;
+				}
+			}
+			reader.reset();
+			//This loop makes groups words into array by their length
+			//First array is 4-letter words second 5-letter and so on.
+			while(true) {
+				if(counter==words) {
+					break;
+				}
+				list = new ArrayList<String>();
+				while((tmp=reader.readLine())!=null) {
+					if(tmp.length()==word_length) {
+						list.add(tmp);
+						counter++;
+					}
+				}
+				reader.reset();
+				word_length++;
+				wordslist.add(list);
+			}
+			reader.close();
+			//This loop checks each word witch every word with length smaller by one if they are "funnelable"
+			//if there are 5 words that match then that is our answer
+			int number_of_words=0;
+			for(int i=1;i<wordslist.size();i++) {
+				//Loop for every n-letter word
+				for(String s1 : wordslist.get(i)) {
+					int number_of_matches=0;
+					//loop to compare previous word with every n-1-letter word
+					for(String s2 : wordslist.get(i-1)) {
+						if(funnel(s1,s2)) {
+							number_of_matches++;
+							if(number_of_matches>=5) {
+								System.out.println(s1);
+								number_of_words++;
+								break;
+							}
+						}
+					}
+				}
+			}
+			System.out.println(number_of_words);
+		}catch (IOException ex) {
+			System.out.println("Missing File");
+		}
 	}
 }
